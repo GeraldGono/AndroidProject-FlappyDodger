820d33340d61acba8270a5e642993fbacd882759
GeraldGono <g.gono@klab.com>
2014-07-15 15:57:00 +0800

    Update Setting.java
    back fucntion to main menu

--- Flappy_Dodger/src/com/geraldgono/flappy_dodger/Setting.java
+++ Flappy_Dodger/src/com/geraldgono/flappy_dodger/Setting.java
@@ -43,10 +43,8 @@ public class Setting extends Activity {
 		}
 	}
 
-	/*
-	 * @Override public boolean onCreateOptionsMenu(Menu menu) { // Inflate the
-	 * menu; this adds items to the action bar if it is present.
-	 * getMenuInflater().inflate(R.menu.setting, menu); return true; }
-	 */
+		public void main(View v) {
+		finish();
 
+	}
 }
