e327fc7539624d3064098d925db92f20259e62ab
GeraldGono <g.gono@klab.com>
2014-07-15 15:26:52 +0800

    issue#1

--- Flappy_Dodger/src/com/geraldgono/flappy_dodger/MainActivity.java
+++ Flappy_Dodger/src/com/geraldgono/flappy_dodger/MainActivity.java
@@ -1,65 +1,38 @@
 package com.geraldgono.flappy_dodger;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+	}
 
+	public void play(View v) {
+		Intent playIntent = new Intent(this, Game.class);
+		startActivity(playIntent);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	public void highscore(View v) {
+		Intent scoreIntent = new Intent(this, HighScore.class);
+		startActivity(scoreIntent);
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+	}
 
-        public PlaceholderFragment() {
-        }
+	public void setting(View v) {
+		Intent settingIntent = new Intent(this, Setting.class);
+		startActivity(settingIntent);
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.activity_main, container, false);
-            return rootView;
-        }
-    }
+	}
+
+	public void exit(View v) {
+		System.exit(0);
+	}
 
 }
